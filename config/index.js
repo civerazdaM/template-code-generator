d71ea5762e088ad41ee3a6cf5568efd977f0e887
Stefan Madzarevic <stev291@gmail.com>
2018-01-13 18:44:04 +0100

    Added default configuration for project specific paths and updated gitignore

---
+++ config/index.js
@@ -0,0 +1,13 @@
+const projectPath = '/Users/stefanmadzarevic/projects/react-redux-starter';
+const projectSourcePath = '/src';
+const projectContainersPath = '/src/containers';
+const projectComponentsPath = '/src/componentsCommon';
+const projectApiPath = '/src/api';
+
+module.exports = {
+  projectPath,
+  projectSourcePath,
+  projectContainersPath,
+  projectComponentsPath,
+  projectApiPath
+};
